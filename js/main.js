44f3574cc430f649496875b31ae68dce585f52e2
Your Name <you@example.com>
2023-02-21 17:22:26 +0100

    ticket 2

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ console.log(age)
 const km = parseInt(prompt("Please tell us how many kilometers you have to travel: ", " "));
 console.log(km)
 
-const price = ` This is the price of your ticket: ( ${km} * 0.21)`;
+let price = ` This is the price of your ticket: ( ${km} * 0.21)`;
 console.log(price)
 if (age >= 65){
-    console.log(price, "${km} * 0.084");
+    price = "km * 0.84";
 }
 if (age <= 18){
-    console.log(price, "${km} * 0.042");
+    price = "km * 0.42";
 }
 
 
